8ddd6c2903908598b12356cfe51f18569695baf6
Gabriel Costa Andrade <108695592+GaabrielCoosta@users.noreply.github.com>
2023-08-22 20:58:49 -0300

    Update USAspending_app.py

--- USAspending_app.py
+++ USAspending_app.py
@@ -24,11 +24,15 @@ st.title("Welcome to the USAspending Explorer!")
 
 st.markdown("""
 This app is designed to make federal spending data publicly available. It reveals the whole picture of government grant funding in a specific state.
+
 It shows the trend of federal spending data in a state in the course of 2012-2022 and it also serves as a tool to understand federal funding at the county level.
 
 This mapping part of the explorer focuses on federal assistance in the form of grants and shows the total funding going to each county. 
-Total grants are obtained by adding up every grant awarded by the federal government to the county. 
-Blank means that the county has no grants in FY 2022 (2022 fiscal year is from Oct. 2021 to Sep. 2022). 
+
+Total grants are obtained by adding up every grant awarded by the federal government to the county.
+
+Blank means that the county has no grants in FY 2022 (2022 fiscal year is from Oct. 2021 to Sep. 2022).
+
 Alaska is not included because the county codes of Alaska extracted from USAspening API do not correspond to the ones provided by the United States Census Bureau.""")
 
 #---------------------------------#
