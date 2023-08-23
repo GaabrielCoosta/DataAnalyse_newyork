744bab26a0e970aeacf7a68b4502c0af3c51719c
Gabriel Costa Andrade <108695592+GaabrielCoosta@users.noreply.github.com>
2023-08-22 21:00:37 -0300

    Update USAspending_app.py

--- USAspending_app.py
+++ USAspending_app.py
@@ -20,7 +20,7 @@ st.set_page_config(layout="wide")
 
 #---------------------------------#
 # Title
-st.title("🇺🇸 Welcome to the USAspending Explorer!")
+st.title(" 🇺🇸 Welcome to the USAspending Explorer!")
 
 st.markdown("""
 This app is designed to make federal spending data publicly available. It reveals the whole picture of government grant funding in a specific state.
